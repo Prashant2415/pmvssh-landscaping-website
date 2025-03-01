d1d18fb20ddb220293b06f2ea13862b43d6625f7
“Prashant2415” <prashantmendhejbp@gmail.com>
2025-03-01 15:00:51 +0530

    Clean up

--- landscaping/src/App.jsx
+++ landscaping/src/App.jsx
@@ -1,49 +1,10 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './App.css'
-import PublicTemplate from './layout/PublicTemplate'
-import About from './pages/About'
-import Home from './pages/Home'
-import Services from './pages/Services'
-import Shop from './pages/Shop'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
+
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <PublicTemplate/>,
-      children: [
-        {
-          path: "/",
-          element: <Home/>
-        },
-        {
-          path: "/about",
-          element: <About/>
-        },
-        {
-          path: "/services",
-          element: <Services/>
-        },
-        {
-          path: "/shop",
-          element: <Shop/>
-        },
-        {
-          path: "/contact",
-          element: <Contact/>
-        },
-        {
-          path: "/login",
-          element: <Login/>
-        }
-      ]
-    }
-  ])
-  return (
-    <RouterProvider router={router}/>
+  return(
+    <h1>Prashant</h1>
   )
 }
-
 export default App
